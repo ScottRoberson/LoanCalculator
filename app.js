758044b332c7553cb79715a871c313315ee08c96
Scott Roberson <srkazan23@gmail.com>
2019-11-06 20:51:26 -0600

    set hide/show functionality with timeout function

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 //Listen for submit
-document.getElementById('loan-form').addEventListener('submit',calculateResults);
+document.getElementById('loan-form').addEventListener('submit',function(e){
+//Hide results
+document.getElementById('results').style.display='none';
+
+document.getElementById('loading').style.display='block';
+
+setTimeout(calculateResults,2000);
+
+  e.preventDefault();
+});
 
 //Calculate Results
-function calculateResults(e){
+function calculateResults(){
 //UI Vars
 const amount = document.getElementById('amount');
 const interest = document.getElementById('interest');
@@ -24,17 +33,29 @@ if(isFinite(monthly)){
   monthlyPayment.value = monthly.toFixed(2);
   totalPayment.value = (monthly * calculatedPayments).toFixed(2);
   totalInterest.value = ((monthly*calculatedPayments)-principal).toFixed(2);
+  
+  //Show results
+  document.getElementById('results').style.display='block';
+
+  //Hide loader
+  document.getElementById('loading').style.display='none';
+
 }else{
   showError('Please check your numbers');
 
 }
 
-e.preventDefault();
-
 }
 
 //Show Error
 function showError(error){
+  
+  //Show results
+  document.getElementById('results').style.display='none';
+
+  //Hide loader
+  document.getElementById('loading').style.display='none';
+  
   //Create a div
   const errorDiv = document.createElement('div');
 
